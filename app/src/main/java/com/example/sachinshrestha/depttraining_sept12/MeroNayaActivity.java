098c882e70244ae7a4c4d8002818f431f9b70656
shr34sachin <shr34.sachin@gmail.com>
2018-09-17 13:09:56 +0545

    2nd Commit-UI Design

---
+++ app/src/main/java/com/example/sachinshrestha/depttraining_sept12/MeroNayaActivity.java
@@ -0,0 +1,13 @@
+package com.example.sachinshrestha.depttraining_sept12;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MeroNayaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mero_naya);
+    }
+}
